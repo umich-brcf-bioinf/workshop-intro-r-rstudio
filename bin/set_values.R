e34ca1c243aa489733744a855d35ffb3d17155c2
Chris Gates <cgates@umich.edu>
2025-03-21 17:35:56 -0400

    Fix ref to Slack channel

--- bin/set_values.R
+++ bin/set_values.R
@@ -3,4 +3,4 @@
 intro_coc_contact = "University of Michigan Equity, Civil Rights, and Title IX Office"
 intro_coc_contact_email = "ecrtoffice@umich.edu"
 ## Slack
-intro_slack_channel = "2025-04-intro-to-r-rstudio"
+intro_slack_channel = "2025-04-intro-r-rstudio"
