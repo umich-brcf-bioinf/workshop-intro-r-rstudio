ae56e1e38b79fd38bdd35858dd54bab518a1834d
Chris Gates <cgates@umich.edu>
2025-03-28 16:39:29 -0400

    Pull URL into set_values; add clairis headshot

--- bin/set_values.R
+++ bin/set_values.R
@@ -1,6 +1,12 @@
 # Intro
+
 ## Code of Conduct
 intro_coc_contact = "University of Michigan Equity, Civil Rights, and Title IX Office"
 intro_coc_contact_email = "ecrtoffice@umich.edu"
+
 ## Slack
 intro_slack_channel = "2025-04-intro-r-rstudio"
+
+rstudio_server_url="http://bfx-workshop02.med.umich.edu"
+
+ssh_download_dns="bfx-workshop01.med.umich.edu"
